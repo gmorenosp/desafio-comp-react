185b144cfc121fe82d4a31e914e85b4c3d7cc367
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-23 19:17:55 -0300

    FeedbackCards Created

---
+++ src/components/FeedbackCard/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function FeedbackCard() {
+    return (
+        <div className="ct-feedback-card-container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+        </div>
+    );
+}
---
+++ src/components/FeedbackCards/index.tsx
@@ -0,0 +1,20 @@
+import FeedbackCard from "../FeedbackCard";
+import "./styles.css";
+
+export default function FeedbackCards() {
+    return (
+        <>
+            <div className="ct-cards-container">
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+            </div>
+        </>
+    );
+}
